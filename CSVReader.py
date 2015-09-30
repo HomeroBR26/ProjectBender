b57dab2965e0a10e4055d2017a8d5745523cf09e
unknown <MAT.TE@ctlmat0267d.Robertshaw.com>
2015-09-30 15:00:42 -0500

    CSVReader.py added.

---
+++ CSVReader.py
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      MAT.TE
+#
+# Created:     20/08/2015
+# Copyright:   (c) MAT.TE 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import csv
+
+def cadFetch():
+    references = []
+    xCoord = []
+    yCoord = []
+    rotation = []
+    partType = []
+    startSaving = False
+
+    with open('009-00723-01C-top.csv', 'rb') as cadFile:
+        reader = csv.reader(cadFile)
+        devices = []
+        device = []
+
+        for row in reader:
+            if row[0] == 'Name':
+                startSaving = True
+                continue
+            if startSaving and (row[0] != '' and not 'H' in row[0]
+            and not 'TP' in row[0]):
+                device.append(['d', row[0].lower(), row[2], row[1], row[3]])
+                devices.append(device)
+            elif startSaving and (row[0] == ''):
+                break
+        return devices
+
+def bomFetch():
+    pass
+
+def main():
+    print cadFetch()
+
+if __name__ == '__main__':
+    main()
